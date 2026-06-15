de4dd406c062edd533502517653d2a8848c0eb3b
Antonio Neto <antonioolimpiodesaneto@gmail.com>
2026-06-15 10:25:59 -0300

    feat(menu): adiciona tela de subsedes

---
+++ src/screens/subsedesScreen.js
@@ -0,0 +1,10 @@
+// src/screens/subsedesScreen.js
+import { View, Text } from "react-native";
+
+export default function SubsedesScreen() {
+  return (
+    <View>
+      <Text>Subsedes da Savóia</Text>
+    </View>
+  );
+}
